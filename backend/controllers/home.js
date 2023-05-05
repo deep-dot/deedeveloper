85653c112099ada32e2c0e89a32771702bb7005a
dee-dot <kuldeepdhillon_1982@yahoo.co.in>
2023-05-05 10:39:23 +1000

    .

--- backend/controllers/home.js
+++ backend/controllers/home.js
@@ -2,7 +2,7 @@
 module.exports = async (req, res) => {
 
     res.render('home.ejs', {
-        style: 'home.css',
+        style: 'home/home.css, home/classH.css, home/classA.css, home/classS.css, home/classT.css, home/classF.css, home/classC.css, home/classM.css, home/why-me.css.',
         bodyId:'HomePage',        
-    })
+    });
 }
