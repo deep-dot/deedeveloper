ab65873b646a016e233d493c8ec2c0bb6ec24fe0
dee-dot <kuldeepdhillon_1982@yahoo.co.in>
2023-05-06 15:13:54 +1000

    .

--- backend/controllers/projects/project-detail.js
+++ backend/controllers/projects/project-detail.js
@@ -2,6 +2,6 @@ module.exports = async(req, res) => {
 
     res.render('pages/components/why-me/projects/project-detail.ejs', {
         style:'project-detail.css',
-        bodyId: 'projectDetailPage'
+        bodyId: 'project-detail'
     });
 }
